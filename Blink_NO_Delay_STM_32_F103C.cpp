444c6571fbe18b55808ac907c5aecbeb41d64cf4
alex-dev-2020 <alex_dev_2020@mail.ru>
2023-07-28 00:11:41 +1000

    created base app structure

--- Blink_NO_Delay_STM_32_F103C.cpp
+++ Blink_NO_Delay_STM_32_F103C.cpp
@@ -1,20 +1,82 @@
 
+
 // Variables:
-int previousMillis = 0;        // will store the last time the LED was updated
-int interval = 1000;            // interval at which to blink (in milliseconds)
+// момент предыдущего  нажатия 
+int previous_push_Start_button_time = 0;        //  момент предыдущего  нажатия
+int confirmed_push_delay = 5000;            // интервал "подтвержденного" нажатия 5 сек.  (in milliseconds)
+
+// настройки 
 void setup() {
-    // Set up the built-in LED pin as output:
-    pinMode(33, OUTPUT);
+    // Входы
+    pinMode(PB9, INPUT);  // контакт кнопки Старт
+    pinMode(PB6, INPUT);  // lub oil pressure  OK    
+    pinMode(PB3, INPUT);  // eng speed OK 
+
+    // Выходы       
+    pinMode(PB7, OUTPUT);   // lub oil pump LED   
+    pinMode(PA15, OUTPUT);  // eng start success LED
+    pinMode(PB5, OUTPUT);    // main Air Switch  LED
+    pinMode(PB5, OUTPUT);   // main Air Fuel Switch LED
+     // pinMode(PA3, OUTPUT); // резерв
+
 }
-void loop() {
-    // Check to see if it's time to blink the LED; that is, if the
-    // difference between the current time and last time we blinked
-    // the LED is bigger than the interval at which we want to blink
-    // the LED.
-    if (millis() - previousMillis > interval) {
-        // Save the last time you blinked the LED
-        previousMillis = millis();
-        // If the LED is off, turn it on, and vice-versa:
-        digitalWrite(33,!digitalRead(33));// Turn the LED from off to on, or on to off
+
+
+// всего  три входа DI
+// уставка по  дребезгу  для всех одна
+#define BUTTON_DEBOUNCE_DELAY 1
+
+// кнопка 
+uint8 isStartButtonPressed(uint8 pin=PB9,
+                      uint32 pressedLevel=HIGH) {
+    if (digitalRead(pin) == pressedLevel) {
+        delay(BUTTON_DEBOUNCE_DELAY);
+        while (digitalRead(pin) == pressedLevel)
+            ;
+        return true;
     }
+    return false;
+}
+
+// давление масла (Switch)
+uint8 isLubOilPressureOK(uint8 pin=PB6,
+                      uint32 pressedLevel=HIGH) {
+    if (digitalRead(pin) == HIGH) {
+        delay(BUTTON_DEBOUNCE_DELAY);
+        while (digitalRead(pin) == HIGH)
+            ;
+        return true;
+    }
+    return false;
+}
+
+// двигатель запустился (Switch)
+uint8 isEngineSpeedOK(uint8 pin=PB3,
+                      uint32 pressedLevel=HIGH) {
+    if (digitalRead(pin) == HIGH) {
+        delay(BUTTON_DEBOUNCE_DELAY);
+        while (digitalRead(pin) == HIGH)
+            ;
+        return true;
+    }
+    return false;
+}
+
+
+// главный  цикл Приложения
+void loop() {
+	
+	
+	// обрабатываем нажатие  кнопки
+    if (isStartButtonPressed()) {
+		// 
+        if (millis() - previous_push_Start_button_time  > confirmed_push_delay) {
+        // сохраняем момент момент предыдущего  нажатия
+        previous_push_Start_button_time = millis();
+     
+        digitalWrite(PB7,HIGH);// Turn the LED from off to on, or on to off
+    }
+				
+  }
+
 }
